cb5f5d021f3416f1698f78eaeab069d09237284f
mbabayi574 <m.babayi574@gmail.com>
2021-09-26 18:26:50 +0330

    fix the navbar animation error

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglemenu() {
+    let toggle = document.querySelector(".toggle");
+    let navigation = document.querySelector('.navigation')
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    console.log("hello");
+
+}
